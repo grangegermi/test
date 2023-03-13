86ef0bea5040e4f2d26921a4ffff67a9e91bcdcf
grangegermi <dasha.voloshina@gmail.com>
2023-03-13 20:41:33 +0300

    Initial Commit

---
+++ task/taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskApp.swift
+//  task
+//
+//  Created by Даша Волошина on 13.03.23.
+//
+
+import SwiftUI
+
+@main
+struct taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
